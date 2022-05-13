3bdbf24db3b640f4911686cd4660e743dfbf7b93
acwell94 <89783182+acwell94@users.noreply.github.com>
2022-05-13 11:23:06 +0900

    Revert "Feature components sejin"

--- src/components/commons/globalstyles/globalstyles.ts
+++ src/components/commons/globalstyles/globalstyles.ts
@@ -1,17 +1,8 @@
 import { css } from "@emotion/react";
 
-
 export const globalstyles = css`
   * {
     margin: 0px;
     box-sizing: border-box;
-    font-family: "pretendard";
-    font-size:20px;
   }
-
-
-@font-face {
-  font-family:"pretendard";
-  src: url("/font/PretendardVariable.woff2");
-}
 `;

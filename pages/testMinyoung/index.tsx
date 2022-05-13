eb69a3205dcf00ede67073af32f72ce54c5243c7
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1 <acwell94@naver.com>
2022-05-13 14:13:10 +0900

    추가 : Store 엔티티 추가

---
+++ pages/testMinyoung/index.tsx
@@ -0,0 +1,9 @@
+import StoreItem from "../../src/components/commons/card/storeCard/StoreItem.presenter";
+
+export default function Test() {
+  return (
+    <>
+      <StoreItem />
+    </>
+  );
+}
---
+++ src/components/units/store/Store.presenter.tsx
@@ -0,0 +1,7 @@
+export default function StorePresenterPage() {
+  return (
+    <div>
+      <div>이곳은 스토어페이지입니다.</div>
+    </div>
+  );
+}

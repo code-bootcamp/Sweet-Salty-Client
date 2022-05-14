ee3f5ba0307df95f388ffd10295cf0cad0f2c44f
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1 <acwell94@naver.com>
2022-05-14 14:27:13 +0900

    추가 : StorePage 추가

--- pages/testMinyoung/index.tsx
+++
@@ -1,9 +0,0 @@
-import StoreCardContainerPage from "../../src/components/commons/card/storeCard/StoreCard.container";
-
-export default function Test() {
-  return (
-    <>
-      <StoreCardContainerPage />
-    </>
-  );
-}

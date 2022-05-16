f1e9b55abe3544f6f7408a0e2052b7b04d40d4d8
eisont <rlaclgns321@naver.com>
2022-05-16 14:09:19 +0900

    추가: MainPage, ReviewDetail, Layout, searchBar, 공통컴포넌트

---
+++ pages/reviewdetail/index.tsx
@@ -0,0 +1,9 @@
+// 리뷰 상세보기 Page  - 김치훈
+
+import ReviewDetail from "../../../src/components/units/detail/reviewdetail/ReviewDetail.container";
+
+// 나중에 동적라우팅 안에 넣어야 한다.
+
+export default function ReviewDetailPage() {
+  return <ReviewDetail />;
+}

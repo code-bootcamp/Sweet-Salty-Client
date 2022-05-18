90054dc6efde5b335896472c2d4af06dd3ba18b0
eisont <rlaclgns321@naver.com>
2022-05-18 15:29:26 +0900

    추가: 공지사항 페이지 추가

---
+++ pages/notice/index.tsx
@@ -0,0 +1,9 @@
+// 공지사항 Page === 김치훈
+
+import NoticeContainerPage from "../../src/components/units/notice/Notice.container";
+
+export default function NoticePage(){
+
+
+  return(<NoticeContainerPage />)
+}

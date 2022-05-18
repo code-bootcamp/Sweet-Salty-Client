f4d6df5c28b0691549a54c27afbbd0d580065218
eisont <rlaclgns321@naver.com>
2022-05-18 20:44:45 +0900

    추가: 공지사항 리스트 추가 -김치훈

---
+++ pages/notice/list/index.tsx
@@ -0,0 +1,9 @@
+// 공지사항 목록 List 페이지 === 김치훈
+
+import NoticeListContainerPage from "../../../src/components/units/notice/list/NoticeList.container";
+
+export default function NoticePage(){
+
+
+  return(<NoticeListContainerPage />)
+}
---
+++ src/components/units/notice/list/NoticeList.container.tsx
@@ -0,0 +1,14 @@
+// 여기는 공지사항 list === 김치훈
+
+import { useRouter } from "next/router";
+import NoticeListPresenterPage from "./NoticeList.presenter";
+
+export default function NoticeListContainerPage() {
+  const router = useRouter()
+  const onClickDetail =() => {
+    router.push("./detail")
+  }
+
+
+  return <NoticeListPresenterPage onClickDetail={onClickDetail} />;
+}

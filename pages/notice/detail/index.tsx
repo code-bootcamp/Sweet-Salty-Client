2a870d5b892b0ab5fc0b30746faebfc061361a74
eisont <rlaclgns321@naver.com>
2022-05-18 20:46:03 +0900

    추가: 공지사항 상세보기 추가 -김치훈

---
+++ pages/notice/detail/index.tsx
@@ -0,0 +1,9 @@
+// 공지사항 detail 페이지 === 김치훈
+
+import NoticeDetailContainer from "../../../src/components/units/notice/detail/NoticeDetail.container";
+
+export default function NoticeDetailPage(){
+
+
+  return <NoticeDetailContainer />
+}

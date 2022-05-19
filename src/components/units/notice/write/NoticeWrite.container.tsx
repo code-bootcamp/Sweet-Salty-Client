4a3c5b4d3db7d69a6a0fd45e9f47466840ee65f2
eisont <rlaclgns321@naver.com>
2022-05-19 12:48:37 +0900

    Revert "추가: 공지사항 등록페이지 폴더 추가  -김치훈"
    This reverts commit 3f6d4d781c0b7a7167e1efdbbcc3f98165e02135.

--- src/components/units/notice/write/NoticeWrite.container.tsx
+++
@@ -1,9 +0,0 @@
-// 공지사항 Writer Container --- 김치훈
-
-import NoticeWritePresenter from "./NoticeWrite.presenter";
-
-export default function NoticeWriteContainer(){
-
-
-  return (<NoticeWritePresenter/>)
-}
--- src/components/units/notice/write/NoticeWrite.queries.ts
+++
@@ -1,1 +0,0 @@
-// 공지사항 Writer Queries --- 김치훈

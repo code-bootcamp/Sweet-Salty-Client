bebade157263660b3faba3858a259e4143b9c760
eisont <rlaclgns321@naver.com>
2022-05-19 09:27:18 +0900

    추가: reviewWritePage, reviewListPage, reviewDetailPage 파일 추가 -김치훈

---
+++ pages/reviews/detail/index.tsx
@@ -0,0 +1,9 @@
+// 일반리뷰 Detail Page  - 김치훈
+
+import ReviewDetailContainer from "../../../src/components/units/detail/ReviewDetail.container";
+
+// 나중에 동적라우팅 안에 넣어야 한다.
+
+export default function ReviewDetailPage() {
+  return <ReviewDetailContainer />;
+}
--- pages/reviews/list/index.tsx
+++ pages/reviews/list/index.tsx
@@ -1,4 +1,4 @@
-// 일반리뷰페이지 === 김치훈
+// 일반리뷰 List 페이지 === 김치훈
 
 import CommonReviewContainerPage from "../../../src/components/units/board/commonReview/CommonReview.container";
 

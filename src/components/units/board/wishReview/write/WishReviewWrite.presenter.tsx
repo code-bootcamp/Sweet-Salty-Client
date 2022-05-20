cd1ccf2d431fecfb403b49acd99be5596ab68941
eisont <rlaclgns321@naver.com>
2022-05-20 18:32:50 +0900

    추가: 가봤어요! Write 페이지 추가 -김치훈

---
+++ src/components/units/board/wishReview/write/WishReviewWrite.presenter.tsx
@@ -0,0 +1,19 @@
+// 가봤어요! Write Presenter -김치훈
+
+import * as S from "./WishReviewWrite.styled";
+
+export default function WishReviewWritePresenterPage() {
+  return (
+
+      <S.Wrapper>
+      <S.Title>가봤어요! 등록</S.Title>
+
+      
+      <S.WishReviewWriteBox>
+
+            <S.RightBoxHr/>
+
+      </S.WishReviewWriteBox>
+    </S.Wrapper>
+  );
+}
---
+++ src/components/units/board/wishReview/write/WishReviewWrite.styled.ts
@@ -0,0 +1,34 @@
+// 가봤어요! Write styles -김치훈
+
+import styled from "@emotion/styled";
+
+
+export const Wrapper = styled.div`
+  padding-bottom: 100px;
+  width: 100%;
+  background: radial-gradient(#ff6e30, #ff9a31);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+export const Title = styled.div`
+  padding: 100px 0;
+  font-size: 40px;
+  font-weight: 700;
+  color: #fff;
+`
+
+export const WishReviewWriteBox = styled.div`
+  padding: 40px;
+  width: 1000px;
+  background: #fff;
+  border-radius: 20px;
+`;
+
+export const RightBoxHr = styled.div`
+  margin: 20px 0;
+  width: 1000px;
+  height: 1.5px;
+  border-radius: 50px;
+  background: #DADADA;
+`

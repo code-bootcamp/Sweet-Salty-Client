f3cf14dcadc741155142d2b05b08fe840c8d9200
sejin <puddingg13579@gmail.com>
2022-05-21 17:43:12 +0900

    추가 : 일반리뷰등록

--- src/components/units/write/CommonReviewWrite.queries.ts
+++ src/components/units/write/CommonReviewWrite.queries.ts
@@ -1,15 +1,15 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_BOARD = gql`
-mutation createBoard($createBoardInput: CreateBoardInput!, $createBoardTagsInput: CreateBoardTagsInput!){
-    createBoard(createBoardInput:$createBoardInput, createBoardTagsInput:$createBoardTagsInput){
-boardId
+mutation createBoard($createBoardInput: CreateBoardInput!, $boardTagsInput: BoardTagsInput!){
+    createBoard(createBoardInput:$createBoardInput, boardTagsInput:$boardTagsInput){
+        boardId
     }
 }
 `;
 
 export const UPDATE_BOARD = gql`
-mutation updateBoard($boardId:String! ,$updateBoardInput: UpdateBoardInput)
+mutation updateBoard($boardId:String! ,$updateBoardInput: UpdateBoardInput!)
 {updateBoard(boardId:$boardId, updateBoardInput:$updateBoardInput){
     boardId
 }}

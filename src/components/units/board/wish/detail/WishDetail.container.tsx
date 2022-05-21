e947be59c6cee9032100a17c1d7e53ac4c5b65c2
eisont <rlaclgns321@naver.com>
2022-05-21 14:52:05 +0900

    추가: 가주세요! Detail 페이지 추가 -김치훈

--- src/components/units/board/wish/detail/WishDetail.container.tsx
+++ src/components/units/board/wish/detail/WishDetail.container.tsx
@@ -1,8 +1,22 @@
 // 가주세요! Detail Container -김치훈
 
+import { useRouter } from "next/router";
 import WishDetailPresenterPage from "./WishDetail.presenter";
 
 
 export default function WishDetailContainerPage() {
-  return <WishDetailPresenterPage />;
+  const router = useRouter()
+
+  const onClickList =()=>{
+    router.push("/reviews/wish/list")
+  }
+  const onClickWishReviewWrite =()=>{
+    router.push("/reviews/wishreview/write")
+  }
+
+
+  return <WishDetailPresenterPage
+    onClickList={onClickList}
+    onClickWishReviewWrite={onClickWishReviewWrite}
+  />;
 }

5f51b075db7dd940d86ab874aa3025d4ad75ecd7
eisont <rlaclgns321@naver.com>
2022-05-21 14:54:59 +0900

    수정: 가봤어요! Write 페이지 경로 수정 -김치훈

--- pages/wishreview/write/index.tsx
+++
@@ -1,9 +0,0 @@
-// 가봤어요! Write 페이지 --- 김치훈
-
-import WishReviewWriteContainerPage from "../../../src/components/units/board/wishReview/write/WishReviewWrite.container"
-
-export default function WishReviewWritePage(){
-
-
-  return <WishReviewWriteContainerPage/>
-}

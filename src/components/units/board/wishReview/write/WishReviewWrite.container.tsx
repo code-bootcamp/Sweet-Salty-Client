81835f27960e86b570be909523f8d43a0d34d451
eisont <rlaclgns321@naver.com>
2022-05-21 17:45:16 +0900

    추가: 가봤어요! Write 페이지 추가 -김치훈

--- src/components/units/board/wishReview/write/WishReviewWrite.container.tsx
+++ src/components/units/board/wishReview/write/WishReviewWrite.container.tsx
@@ -1,7 +1,20 @@
 // 가봤어요! Write Container -김치훈
 
+import { useRouter } from "next/router";
 import WishReviewWritePresenterPage from "./WishReviewWrite.presenter";
 
 export default function WishReviewWriteContainerPage() {
-  return <WishReviewWritePresenterPage />;
+  const router = useRouter()
+
+  const onClickCommonReviewList =() =>{
+    alert("가봤어요! 등록되었습니다.")
+    router.push("/reviews/wishreview/detail")
+  }
+  const onClickBack = ()=>{
+    router.back()
+  }
+  return <WishReviewWritePresenterPage
+  onClickCommonReviewList={onClickCommonReviewList}
+    onClickBack={onClickBack}
+  />;
 }

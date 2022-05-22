e34e389be25dab8edf82aebf8eb8a4fc326b30ae
eisont <rlaclgns321@naver.com>
2022-05-22 20:18:47 +0900

    추가: 마이페이지 회원 정보 변경 추가 -김치훈

---
+++ pages/mypage/infomodify/index.tsx
@@ -0,0 +1,8 @@
+// mypage 회원 정보 변경 Page --- 김치훈
+
+import ModifyContainerPage from "../../../src/components/units/mypage/infomodify/Modify.container";
+
+export default function MypageInfoModifyPage(){
+
+  return (<ModifyContainerPage/>)
+}
---
+++ src/components/units/mypage/infomodify/Modify.container.tsx
@@ -0,0 +1,8 @@
+// mypage 회원 정보 변경 Container --- 김치훈
+
+import ModifyPresenterPage from "./Modify.presenter";
+
+export default function ModifyContainerPage(){
+
+  return <ModifyPresenterPage/>
+}

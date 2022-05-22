d6392e9b41340092ceb5a771a01433025edf3d5d
eisont <rlaclgns321@naver.com>
2022-05-22 20:17:22 +0900

    수정: 공동컴포넌트 reviewMenu 수정(들어간 페이지 background,color 지정) -김치훈

--- src/components/commons/reviewMenu/index.tsx
+++ src/components/commons/reviewMenu/index.tsx
@@ -17,8 +17,22 @@ export const ReviewMenu = styled.div`
   border-radius: 20px 20px 0 0;
   font-weight: 700;
   font-size: 20px;
-  color: #7b7b7b;
-  background: #fff;
+  color:${(props: any)=> 
+    props.isReviewList || 
+    props.isCommonReviewList ||
+    props.isTesterReview ||
+    props.isWishList 
+    ? "#fff"
+    : "#7b7b7b" 
+  };
+  background: ${(props: any)=> 
+    props.isReviewList || 
+    props.isCommonReviewList ||
+    props.isTesterReview ||
+    props.isWishList 
+    ? "linear-gradient(#ff6e30, #ffa230)"
+    : "#fff" 
+  };
   cursor: pointer;
   transition: 0.2s;
   &:hover {
@@ -29,6 +43,20 @@ export const ReviewMenu = styled.div`
 
 export default function ReviewMenuPage() {
   const router = useRouter();
+
+  
+  // 전체리뷰 
+  const ReviewList = ["/reviews"];
+  const isReviewList = ReviewList.includes(router.asPath);
+  // 단짠리뷰 
+  const CommonReviewList = ["/reviews/commonReview/list"];
+  const isCommonReviewList = CommonReviewList.includes(router.asPath);
+  // 시식단리뷰
+  const TesterReview = ["/reviews/testerReview/list"];
+  const isTesterReview = TesterReview.includes(router.asPath);
+  // 거주세요! 리뷰
+  const WishList = ["/reviews/wish/list"];
+  const isWishList = WishList.includes(router.asPath);
 
   const onClickCommunityList = () => {
     router.push("/reviews");
@@ -47,10 +75,10 @@ export default function ReviewMenuPage() {
   return (
     <>
       <ReviewMenuBox>
-        <ReviewMenu onClick={onClickCommunityList}>전체</ReviewMenu>
-        <ReviewMenu onClick={onClickCommonReviewList}>단짠 리뷰</ReviewMenu>
-        <ReviewMenu onClick={onClickTasterReviewList}>시식단 리뷰</ReviewMenu>
-        <ReviewMenu onClick={onClickWishReviewList}>가주세요!</ReviewMenu>
+        <ReviewMenu onClick={onClickCommunityList} isReviewList={isReviewList}>전체</ReviewMenu>
+        <ReviewMenu onClick={onClickCommonReviewList} isCommonReviewList={isCommonReviewList}>단짠 리뷰</ReviewMenu>
+        <ReviewMenu onClick={onClickTasterReviewList} isTesterReview={isTesterReview}>시식단 리뷰</ReviewMenu>
+        <ReviewMenu onClick={onClickWishReviewList} isWishList={isWishList}>가주세요!</ReviewMenu>
       </ReviewMenuBox>
     </>
   );

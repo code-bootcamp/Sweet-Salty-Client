7684f9d00741b8f96d65a7a1089aa85e9a9abdf2
eisont <rlaclgns321@naver.com>
2022-05-25 22:23:26 +0900

    추가: MypageLike 페이지 추가 ---김치훈

---
+++ pages/mypage/mypagelike/index.tsx
@@ -0,0 +1,10 @@
+// MypageLike page --- 김치훈
+
+import MypageLikeContainer from "../../../src/components/units/mypage/mypagelike/MypageLike.container";
+
+
+export default function MypageLikePage() {
+  
+
+  return <MypageLikeContainer />;
+}
---
+++ src/components/units/mypage/mypagelike/MypageLike.container.tsx
@@ -0,0 +1,10 @@
+// MypageLike Container --- 김치훈
+
+import MypageLikePresenter from "./MypageLike.presenter";
+
+
+export default function MypageLikeContainer() {
+  
+
+  return <MypageLikePresenter />;
+}

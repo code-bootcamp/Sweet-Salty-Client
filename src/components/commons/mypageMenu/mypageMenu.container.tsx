fcccff27f6155a251c2f91f3291560a49da43001
eisont <rlaclgns321@naver.com>
2022-05-25 22:21:31 +0900

    추가: MypageMenu 공동컴포넌트 추가 ---김치훈

---
+++ src/components/commons/mypageMenu/mypageMenu.container.tsx
@@ -0,0 +1,36 @@
+// mypageMenu Container --- 김치훈
+
+import { useRouter } from "next/router";
+import MyPageMenuPresenter from "./mypageMenu.presenter";
+
+
+export default function MyPageMenuContainer() {
+  const router = useRouter()
+
+  const mypage = ["/mypage"]
+  const ismypage = mypage.includes(router.pathname);
+  const mypageLike = ["/mypage/mypagelike"]
+  const ismypageLike = mypageLike.includes(router.pathname);
+  const mypagePoint = ["/mypage/mypagepoint"]
+  const ismypagePoint = mypagePoint.includes(router.pathname);
+
+  const onClickMypage =()=>{
+    router.push("./../mypage")
+  }
+  const onClickMypageLike =()=>{
+    router.push("./../mypage/mypagelike")
+  }
+  const onClickMypagePoint =()=>{
+    router.push("./../mypage/mypagepoint")
+  }
+  
+
+  return <MyPageMenuPresenter 
+  ismypage={ismypage}
+  ismypageLike={ismypageLike}
+  ismypagePoint={ismypagePoint}
+  onClickMypage={onClickMypage}
+  onClickMypageLike={onClickMypageLike}
+  onClickMypagePoint={onClickMypagePoint}
+  />;
+}

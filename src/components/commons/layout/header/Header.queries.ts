f0cd3deea47d877933f7447fe905c6b84c3f2781
eisont <rlaclgns321@naver.com>
2022-05-26 17:40:54 +0900

    추가: 로그인 유저 정보 추가 ---김치훈

--- src/components/commons/layout/header/Header.queries.ts
+++ src/components/commons/layout/header/Header.queries.ts
@@ -3,3 +3,23 @@ import { gql } from "@apollo/client";
 export const LOGOUT = gql`
 mutation logout{logout}
 `;
+
+export const FETCH_USER_LOGGED_IN =gql`
+query fetchUserLoggedIn{
+    fetchUserLoggedIn{
+      userId
+      userState
+      userEmail
+      userName
+      userNickname
+      userImage
+      userProfile
+      userPhone
+      userPoint
+      ageGroup
+      gender
+      createAt
+      updateAt
+    }
+  }
+`;

f4e3d82417a85e149c2fcf507d8f5cfd14a888b5
eisont <rlaclgns321@naver.com>
2022-05-27 18:31:29 +0900

    추가: 좋아요 한 단짠 게시글 카드 추가 ---김치훈

---
+++ src/components/commons/card/LikeReviewCard/LikeReviewCard.styles.ts
@@ -0,0 +1,109 @@
+import { HeartFilled } from "@ant-design/icons";
+import styled from "@emotion/styled";
+
+
+export const ItemMainDiv = styled.div`
+  margin-bottom: 15px;
+  height: 341px;
+  width: 338px;
+`;
+// 썸네일 사진
+export const ItemImg = styled.div`
+  padding: 15px;
+  width: 338px;
+  height: 250px;
+  background-color: #D2D2D2;
+  background: no-repeat url(${(props: any) => props.el?.thumbnail 
+    ? `https://storage.googleapis.com/${props.el?.thumbnail}`
+    : "https://t1.daumcdn.net/cfile/tistory/24283C3858F778CA2E"
+  });
+  background-size: cover;
+  background-position: center;
+
+  border-radius: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: start;
+`;
+export const BoardSubject = styled.div`
+  font-size: 18px;
+  font-weight: 700;
+  color: #fff;
+`
+// 포로필사진
+export const ItemProfile = styled.img`
+  height: 40px;
+  width: 40px;
+  background-color: #fff;
+  border-radius: 50%;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+`;
+export const ItemInfoDiv = styled.div`
+  width: 338px;
+`;
+export const FlexBox = styled.div`
+  padding-top: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const FlexBox2 = styled.div`
+  display: flex;
+`;
+export const ItemInfoTitleDiv = styled.div`
+  color: #676767;
+  font-size: 18px;
+  font-weight: 700;
+`;
+export const ItemInfoUserDiv = styled.div`
+  color: #2c2c2c;
+  font-size: 14px;
+  font-weight: 500;
+`;
+export const Span = styled.span`
+  font-size: 14px;
+`;
+
+export const ItemInfoMidDiv = styled.div`
+  padding-top: 10px;
+  display: flex;
+  align-items: center;
+  color: #676767;
+`;
+export const ItemInfoStoreName = styled.div`
+  font-size: 14px;
+  font-weight: 500;
+`;
+export const P = styled.div`
+  margin: 0 5px;
+  background: #676767;
+  width: 1px;
+  height: 14px;
+  `;
+export const ItemInfoLocationDiv = styled.div`
+  color: #676767;
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+
+export const ItemInfoBottomDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+export const LikeFillIcon = styled(HeartFilled)`
+  font-size: 24px;
+  color: #FF9A31;
+`;
+export const ItemInfoImg = styled.img`
+  height: 24px;
+`;
+export const ItemInfoCountDiv = styled.div`
+  color: #2c2c2c;
+  font-size: 14px;
+  padding: 0 5px;
+`;
+export const IteminfoDateDiv = styled.div`
+  color: #676767;
+  font-size: 12px;
+`;

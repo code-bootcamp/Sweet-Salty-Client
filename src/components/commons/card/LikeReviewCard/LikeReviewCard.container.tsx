738bbaeabb56b3544d1abdee73d322a2dd3cdef4
eisont <rlaclgns321@naver.com>
2022-05-27 22:22:58 +0900

    추가: 좋아요 한 단짠게시글 상세보기 연결 코드 추가 ---김치훈

--- src/components/commons/card/LikeReviewCard/LikeReviewCard.container.tsx
+++ src/components/commons/card/LikeReviewCard/LikeReviewCard.container.tsx
@@ -1,5 +1,9 @@
 import LikeReviewCardPresenter from "./LikeReviewCard.presenter";
 
 export default function LikeReviewCardContainer(props: any) {
-  return <LikeReviewCardPresenter el={props.el} data={props.data} />;
+  return <LikeReviewCardPresenter 
+    el={props.el} 
+    data={props.data} 
+    onClickMoveLikeReviewDetail={props.onClickMoveLikeReviewDetail}
+    />;
 }

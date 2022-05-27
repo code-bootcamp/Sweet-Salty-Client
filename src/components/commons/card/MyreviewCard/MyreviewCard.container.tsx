076d165c9910e3f04600d8130f75a404eb128765
eisont <rlaclgns321@naver.com>
2022-05-27 22:23:19 +0900

    추가: 마이단짠 게시글 상세보기 연결 코드 추가 ---김치훈

--- src/components/commons/card/MyreviewCard/MyreviewCard.container.tsx
+++ src/components/commons/card/MyreviewCard/MyreviewCard.container.tsx
@@ -1,5 +1,9 @@
 import MyreviewCardPresenter from "./MyreviewCard.presenter";
 
 export default function MyreviewCardContainer(props: any) {
-  return <MyreviewCardPresenter el={props.el} data={props.data} />;
+  return <MyreviewCardPresenter 
+    el={props.el} 
+    data={props.data} 
+    onClickMoveMyReviewDetail={props.onClickMoveMyReviewDetail}
+  />;
 }

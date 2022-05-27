9706d35926a4887d813616433ab4e6e961c3b048
eisont <rlaclgns321@naver.com>
2022-05-27 22:20:15 +0900

    추가: Mypage 충전 컴포넌트 추가 ---김치훈

---
+++ src/components/commons/Mypage/ChargeComponent/chargecomponent.container.tsx
@@ -0,0 +1,9 @@
+// 충전 상세내역 Component container ---김치훈
+
+import ChargeComponentPresenter from "./chargecomponent.presenter";
+
+export default function ChargeComponentContainer (){
+
+
+  return <ChargeComponentPresenter/>
+}

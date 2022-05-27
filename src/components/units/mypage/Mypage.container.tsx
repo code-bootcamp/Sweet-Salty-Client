2fa6ac36a8918ad73f9dd43bdf800f50675c0803
eisont <rlaclgns321@naver.com>
2022-05-27 18:35:09 +0900

    추가: 마이단짠게시글 페이지 게시글 추가 ---김치훈

--- src/components/units/mypage/Mypage.container.tsx
+++ src/components/units/mypage/Mypage.container.tsx
@@ -9,16 +9,38 @@ import { FETCH_BOARDS_OF_USER, FETCH_USER_LOGGED_IN } from "./Mypage.queries";
 export default function MyPageContainer() {
 
   const {data: loggedIn} = useQuery(FETCH_USER_LOGGED_IN)
-
-  const {data} =useQuery(FETCH_BOARDS_OF_USER, {
+  
+  // 마이단짠 게시글 목록
+  const {data, refetch, fetchMore} =useQuery(FETCH_BOARDS_OF_USER, {
     variables: {
       userNickname: String(loggedIn?.fetchUserLoggedIn?.userNickname)
     }
   })
 
+  const onLoadMore = () => {
+    if (!data) return;
+
+    fetchMore({
+      variables: { page: Math.ceil(data?.fetchBoardsOfUser.length / 5) + 1 },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        // 받아올 데이터가 없을 경우 return(기존 데이터 보여줘)
+        if (!fetchMoreResult?.fetchBoardsOfUser) return { fetchBoardsOfUser: [...prev.fetchBoardsOfUser] };
+        return {
+          fetchBoardsOfUser: [
+            // 이전 뎃글 보여줘
+            ...prev.fetchBoardsOfUser,
+            // 다음 댓글 보여줘
+            ...fetchMoreResult.fetchBoardsOfUser,
+          ],
+        };
+      },
+    });
+  };
   
 
   return <MyPagePresenter
     data={data}
+    refetch={refetch}
+    onLoadMore={onLoadMore}
   />;
 }
--- src/components/units/mypage/Mypage.presenter.tsx
+++ src/components/units/mypage/Mypage.presenter.tsx
@@ -5,8 +5,11 @@ import * as S from "./Mypage.styled";
 import MypageUserInfoContainer from "../../commons/UserInfo/userInfo.container";
 import UserMenuContainer from "../../commons/UserMenu/userMenu.container";
 import MyreviewCardContainer from "../../commons/card/MyreviewCard/MyreviewCard.container";
+import { v4 as uuidv4 } from "uuid";
+// import InfiniteScroll from 'react-infinite-scroller';
 
 export default function MyPagePresenter(props: any) {
+  console.log(props.data?.fetchBoardsOfUser)
   return (
     <S.Wrapper>
       <S.Title>마이페이지</S.Title>
@@ -15,13 +18,28 @@ export default function MyPagePresenter(props: any) {
       <MypageUserInfoContainer />
 
       {/* mypage 메뉴 */}
-      <UserMenuContainer data={props.data}/>
+      <UserMenuContainer />
+
 
       <S.MypageSectionBox>
-
-        {/* <MyreviewCardContainer/> */}
-
+        {/* <InfiniteScroll pageStart={0} loadMore={props.onLoadMore} hasMore={true} useWindow={true}> */}
+          <S.TableTop>
+            {props?.data?.fetchBoardsOfUser.map((el: any)=>
+              <S.MypageBoardsOfUser key={uuidv4()}>
+                <MyreviewCardContainer el={el} data={props.data}/>
+              </S.MypageBoardsOfUser>
+            )}
+          </S.TableTop>
+        {/* </InfiniteScroll> */}
       </S.MypageSectionBox>
+
+      {/* <S.MypageSectionBox>
+        {props?.data?.fetchBoardsOfUser.map((el: any)=>
+          <S.MypageBoardsOfUser key={uuidv4()}>
+            <MyreviewCardContainer el={el} data={props.data}/>
+          </S.MypageBoardsOfUser>
+        )}
+      </S.MypageSectionBox> */}
       <S.TopButtonBox>
         <TopButton/>
       </S.TopButtonBox>

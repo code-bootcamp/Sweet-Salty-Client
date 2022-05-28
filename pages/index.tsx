85be9b317ac6ba55fdc59b2aac0ad7e649788c11
sejin <puddingg13579@gmail.com>
2022-05-28 22:05:29 +0900

    추가:메인페이지 상단 버튼 색 빼고 완료

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-// 메인 페이지 - 김치훈
 
 import MainPageList from "../src/components/units/mainpage/Mainpage.container";
 
--- src/components/units/mainpage/Mainpage.container.tsx
+++ src/components/units/mainpage/Mainpage.container.tsx
@@ -7,7 +7,6 @@ import { FETCH_PREFER_BOARDS, FETCH_RECENT_BOARDS, FETCH_USER_LOGGED_IN, REAL_TI
 export default function MainPageList() {
   const router = useRouter();
   const {data : loggedInData} = useQuery(FETCH_USER_LOGGED_IN);
-  const [category , setCategory] = useState("REVIEW")
   const {data : recentBoardsData } =useQuery(FETCH_RECENT_BOARDS,{
     variables: { category}
   });
@@ -16,8 +15,11 @@ export default function MainPageList() {
   });
   const {data : preferData} = useQuery(FETCH_PREFER_BOARDS)
   const {data: recentShopData} = useQuery(REAL_TIME_SHOP)
+  const [category , setCategory] = useState("REVIEW")
+  // 
+  // const [tab, setTab] = useState("cur")
+  // 
   const loggedInNickname = loggedInData?.fetchUserLoggedIn?.userNickname
-  
   const onClickCetegory =(event)=>{
     setCategory(event.currentTarget.id)
   }
@@ -32,41 +34,24 @@ export default function MainPageList() {
   const onClickShopPage=()=>{
     router.push(`/shop`)
   }
-const onClickDetailPage =(id)=>()=>{
-  if(category==="REVIEW")
-  router.push(`/reviews/commonReview/${id}`)
-  if(category==="REQUEST")
-  router.push(`/reviews/wish/${id}`)
-  if(category==="VISITED")
-  router.push(`/reviews/wishreview/${id}`)
-  if(category==="TASTER")
-  router.push(`/reviews/tasterReview/${id}`)
+const onClickLogin = () => {
+  router.push(`/login`)
 }
-
-
-  // 단짠리뷰 
-  const CommonReviewList = ["/reviews/commonReview"];
-  const isCommonReviewList = CommonReviewList.includes(router.asPath);
-  // 가봤어요! 리뷰
-  const VisitedReview = ["/reviews/visitedReview"];
-  const isVisited = VisitedReview.includes(router.asPath);
-  // 가주세요! 리뷰
-  const WishList = ["/reviews/wish"];
-  const isWishList = WishList.includes(router.asPath);
-
+const onClickSignUp = () => {
+  router.push(`/signup`)
+}
   return <MainpagePresenter
           onClickReview={onClickReview}
           onClickTasterPage={onClickTasterPage}
           onClickShopPage={onClickShopPage}
-          loggedInNickname={loggedInNickname}
           onClickCetegory={onClickCetegory}
-          onClickDetailPage={onClickDetailPage}
-          isCommonReviewList={isCommonReviewList}
-          isVisited={isVisited}
-          isWishList={isWishList}
+          onClickLogin={onClickLogin}
+          onClickSignUp ={onClickSignUp}
+          loggedInNickname={loggedInNickname}
           recentBoardsData={recentBoardsData}
           recentShopData={recentShopData}
           recentBoardsTasterData={recentBoardsTasterData}
+          preferData={preferData}
           category={category}
           />;
 }

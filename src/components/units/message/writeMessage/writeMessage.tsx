328238473399e61c714793151eeef8648b97ae32
eisont <rlaclgns321@naver.com>
2022-05-28 23:12:14 +0900

    수정: 쪽지 보내기 styles 수정 ---김치훈

--- src/components/units/message/writeMessage/writeMessage.tsx
+++ src/components/units/message/writeMessage/writeMessage.tsx
@@ -13,7 +13,7 @@ export default function WriteMessagePage(){
     const onClickCancel = ()=>{
         router.push("/message/received")  
     }
-    const onClickSubmit = async(data)=>{
+    const onClickSubmit = async(data: any)=>{
         try{
             await sendMessage({
                 variables: {
@@ -31,16 +31,23 @@ export default function WriteMessagePage(){
     }
     return(
         <S.MessageBoxDiv>
-            <form onSubmit={handleSubmit(onClickSubmit)}>
-            <S.MenuTxt>받는 사람</S.MenuTxt>
-            <S.ReceiveUserInput {...register("receiveUser")} type="text" placeholder="쪽지를 받는 단짝님의 닉네임을 작성하세요."/>
-            <S.MenuTxt>내용</S.MenuTxt>
-            <S.MessageTextarea {...register("contents")}  />
-            <S.BtnBoxDiv>
-            <S.SendBtn type="submit">보내기</S.SendBtn>
-            <S.CancelBtn type="button" onClick={onClickCancel}>취소하기</S.CancelBtn>
-            </S.BtnBoxDiv>
-            </form>
+            <S.Title>쪽지함 <S.RightOutline />쪽지보내기</S.Title>
+            <S.Wrapper>
+                <form onSubmit={handleSubmit(onClickSubmit)}>
+                    <S.MenuTxt>받는 단짝님</S.MenuTxt>
+                        <S.ReceiveUserInput {...register("receiveUser")} type="text" placeholder="쪽지를 받는 단짝님의 닉네임을 작성하세요."/>
+
+                        <S.MessageHr />
+
+                        <S.MenuTxt>내용</S.MenuTxt>
+                        <S.MessageTextarea {...register("contents")} placeholder="안녕하세요, 단짠 맛집의 단짝님! &#13;&#10;단짝 스토어에서 원하는 메뉴를 처음 구매 시, 100 포인트를 적립할 수 있는 이벤트를 진행하고 있습니다. &#13;&#10;많은 관심 부탁드립니다. 감사합니다 :)" />
+                        <S.BtnBoxDiv>
+                            <S.SendBtn type="submit">보내기</S.SendBtn>
+                            <S.CancelBtn type="button" onClick={onClickCancel}>취소하기</S.CancelBtn>
+                    </S.BtnBoxDiv>
+                </form>
+            </S.Wrapper>
         </S.MessageBoxDiv>
     )
 }
+

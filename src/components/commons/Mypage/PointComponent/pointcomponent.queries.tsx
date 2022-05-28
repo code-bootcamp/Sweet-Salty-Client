7cf2b1af4adbc7acdd9fec4c00310cf6e4700671
eisont <rlaclgns321@naver.com>
2022-05-28 20:13:52 +0900

    추가: 결제 상세내역 임시 기능 추가 ---김치훈

--- src/components/commons/Mypage/PointComponent/pointcomponent.queries.tsx
+++ src/components/commons/Mypage/PointComponent/pointcomponent.queries.tsx
@@ -1,1 +1,20 @@
 // 결제 상세내역 Component queries ---김치훈
+
+import { gql } from "@apollo/client"
+
+
+export const FETCH_PAYMENT_HISTORY = gql`
+  query fetchPaymentHistory {
+    fetchPaymentHistory{
+      paymentHistoryId
+      payStatus
+      productStatus
+      supplier
+      paymentAmount
+      stock
+      barcode
+      createdAt
+    }
+  }
+`
+

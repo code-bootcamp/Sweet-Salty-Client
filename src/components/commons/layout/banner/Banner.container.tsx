fe1368be5519ed8d500812f598d79d72796f3fb9
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1 <acwell94@naver.com>
2022-05-31 02:43:16 +0900

    수정 : 배너 수정

--- src/components/commons/layout/banner/Banner.container.tsx
+++ src/components/commons/layout/banner/Banner.container.tsx
@@ -7,10 +7,14 @@ import Slider from "react-slick";
 const Slick = styled(Slider)`
   width: 100%;
   .slick-list {
+    width: 100%;
   }
-  .slick-slide div{
+  .slick-slide div {
   }
   .slick-dots {
+    display: flex;
+    justify-content: center;
+    width: 100%;
     bottom: 20px;
   }
   .slick-track {
@@ -31,34 +35,28 @@ const Slick = styled(Slider)`
   }
 `;
 const settings = {
-  // 좌우 화살표 보여주기
   arrows: false,
-  // 무제한으로 돌릴꺼야?
   infinite: true,
-  // 넘어가는 속도
   speed: 500,
-  // 사진 몇개 보여줄꺼야?
   slidesToShow: 1,
-  // 몇 개씩 넘길꺼야?
   slidesToScroll: 1,
   dots: true,
   autoplay: true,
   autoplaySpeed: 3000,
   cssEase: "ease",
-
 };
 
 export default function LayoutBanner() {
   return (
     <S.BannerWrapper>
       <Slick {...settings}>
-        <img src="/images/banner/Banner_1.png"/>
+        <img src="/images/banner/Banner_1.png" />
 
-        <img src="/images/banner/Banner_2.png"/>
+        <img src="/images/banner/Banner_2.png" />
 
-        <img src="/images/banner/Banner_3.png"/>
+        <img src="/images/banner/Banner_3.png" />
 
-        <img src="/images/banner/Banner_4.png"/>
+        <img src="/images/banner/Banner_4.png" />
       </Slick>
     </S.BannerWrapper>
   );
